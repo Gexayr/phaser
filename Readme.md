74806b5e66e800c9cefcdd615bf46c761facf5cd
Gexayr Melkonyan <mgexayr@smartbet.am>
2023-08-10 15:22:59 +0400

    scale objects and balls and move them

---
+++ Readme.md
@@ -0,0 +1,30 @@
+# Phaser.js Zuma Game
+
+## Description
+This project is a Zuma game built using Phaser.js, a popular HTML5 game development framework.
+
+## Installation
+To install the project dependencies, run the following command:
+```
+npm install
+```
+
+## Build
+To build the project for production, use the following command:
+```
+npm run build
+```
+This will compile and bundle your game into a `main.js` file in the `/dist` directory.
+
+## Start
+To start the game in development mode and access it in your browser, use the following command:
+```
+npm start
+```
+This will start a local development server and open the game in your default browser. You can access the game at `http://localhost:8080` or the specified port.
+
+## Credits
+- Phaser.js: [https://phaser.io](https://phaser.io)
+
+## License
+This project is licensed under the [MIT License](LICENSE.txt).
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -25,6 +25,7 @@ export default class GameScene extends Phaser.Scene {
 
         this.load.image('background', '../assets/road_background_front_port.png');
 
+        this.load.image('background', '../assets/road_background_front_port.png');
         this.load.image('cannon', '../assets/obj.png');
         this.load.image('ball', '../assets/Ball_Blue.png');
     }
@@ -32,7 +33,20 @@ export default class GameScene extends Phaser.Scene {
     create() {
         console.log('game.create')
         // Create our game objects here.
-        this.cannon = this.physics.add.sprite(400, 500, 'cannon');
+
+        // Center coordinates
+        const centerX = this.scale.width * 0.5;
+        const centerY = this.scale.height * 0.5;
+
+
+        const background = this.add.image(0, 0, 'background').setOrigin(0, 0);
+        background.setScale(this.scale.width / background.width, this.scale.height / background.height);
+
+
+        this.cannon = this.physics.add.sprite(centerX, centerY, 'cannon');
+        // Set the scale of the cannon
+        this.cannon.setScale(0.4); // Adjust the scale value to make the cannon smaller
+
         this.physics.world.enable(this.cannon);
     }
 
@@ -61,10 +75,13 @@ export default class GameScene extends Phaser.Scene {
         const ball = this.physics.add.sprite(this.cannon.x, this.cannon.y, 'ball');
 
         // Set the velocity of the ball to launch it in the direction of the cannon's angle
-        const angle = Phaser.Math.DegToRad(this.cannon.angle);
+        const angle = Phaser.Math.DegToRad(this.cannon.angle - 90);
         const velocity = new Phaser.Math.Vector2(Math.cos(angle), Math.sin(angle)).normalize().scale(500);
 
-        this.physics.velocityFromRotation(angle, 500, ball.body.velocity as Phaser.Math.Vector2); // Cast the argument to Vector2
+        this.physics.velocityFromRotation(angle, 500, ball.body.velocity as Phaser.Math.Vector2);
+
+        // Set the scale of the ball
+        ball.setScale(0.2); // Adjust the scale value to make the ball smaller
 
     }
 }
