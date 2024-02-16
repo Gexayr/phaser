91884f567954775a20ca18d0fa02ec10a50b0117
Gexayr Melkonyan <mgexayr@smartbet.am>
2024-02-16 13:06:51 +0400

    get works on server v2

--- webpack.config.js
+++ webpack.config.js
@@ -20,15 +20,6 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/'
     },
-    mode: 'development',
-    devServer: {
-        // open: true,
-        port: 8080,
-        static: {
-            directory: path.join(__dirname, '/'), // rather than contentBase
-            watch: true,
-        },
-    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
