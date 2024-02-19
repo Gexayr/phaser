7cc6b3244d727ec9f73b4fabed0eea7a98e69de3
Gexayr Melkonyan <mgexayr@smartbet.am>
2024-02-19 18:02:59 +0400

    menu background

--- src/scenes/PreloadScene.ts
+++ src/scenes/PreloadScene.ts
@@ -11,7 +11,8 @@ export default class PreloadScene extends Phaser.Scene {
         console.log('preload.preload')
 
         // Load all your assets here
-        this.load.image('background', '../assets/road_background_front_port.png');
+        // this.load.image('background', '../assets/road_background_front_port.png');
+        this.load.image('background', 'https://raw.githubusercontent.com/Gexayr/phaser/main/assets/road_background_front_port.png');
 
         // After assets load, start the MenuScene
         this.load.on('complete', () => {
