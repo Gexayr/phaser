1dff036c2a3433dfa12e2edf3ddbca487a4816a2
Gexayr Melkonyan <mgexayr@smartbet.am>
2024-03-19 16:28:27 +0400

    get data from backEnd v3

--- src/scenes/MenuScene.ts
+++ src/scenes/MenuScene.ts
@@ -35,10 +35,18 @@ export default class MenuScene extends Phaser.Scene {
         background.displayWidth = gameWidth;
         background.displayHeight = gameHeight;
 
+        const buttonStyle = {
+            color: '#ffffff', // Text color
+            fontFamily: 'Arial', // Font family
+            fontSize: 20, // Font size
+            backgroundColor: '#4CAF50', // Button background color
+            padding: { x: 20, y: 10 }, // Padding around the text
+        }
         // Create the play button text
-        this.playButton = this.add.text(gameWidth / 2, gameHeight / 2, 'Play Game', { color: '#0f0' })
+        this.playButton = this.add.text(gameWidth / 2, gameHeight / 2, 'Play Game', buttonStyle)
             .setInteractive()
             .on('pointerdown', () => this.startGame());
+
 
         // Center the play button
         this.playButton.setOrigin(0.5);
@@ -46,6 +54,13 @@ export default class MenuScene extends Phaser.Scene {
         // Adjust the font size based on the screen size
         const fontSize = Math.min(gameWidth * 0.05, gameHeight * 0.05);
         this.playButton.setFontSize(fontSize);
+
+        // Create text objects for displaying user balance and username
+        const balanceText = this.add.text(gameWidth / 2, gameHeight / 2 + 50, 'Balance: ', { color: '#0f0' });
+        balanceText.setOrigin(0.5);
+
+        const usernameText = this.add.text(gameWidth / 2, gameHeight / 2 + 100, 'Username: ', { color: '#0f0' });
+        usernameText.setOrigin(0.5);
 
         // Establish Socket.IO connection
         this.socket = io('http://localhost:8010');
@@ -62,12 +77,20 @@ export default class MenuScene extends Phaser.Scene {
                 localStorage.setItem('uuid', response?.uuid);
             }
             console.log('Socket.IO userData:', response);
+            // Update balance and username text based on received data
+            if (!!response?.balance) {
+                balanceText.setText(`Balance: ${response?.balance}`);
+            }
+            if (!!response?.username) {
+                usernameText.setText(`Username: ${response?.username}`);
+            }
         });
 
         this.socket.on('error', (error: any) => {
             console.error('Socket.IO error:', error);
         });
     }
+
 
     startGame() {
         // Switch to the GameScene
